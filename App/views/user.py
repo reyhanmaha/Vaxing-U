332f6e36aafbf5801b7a607870055c35ed0a5050
reyhanmaha <reyhan12maharaj@gmail.com>
2022-04-01 14:56:13 -0400

    users changes

--- App/views/user.py
+++ App/views/user.py
@@ -11,20 +11,20 @@ from App.controllers import (
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
 
-@user_views.route('/users', methods=['GET'])
-def get_user_page():
-    users = get_all_users()
-    return render_template('users.html', users=users)
+#@user_views.route('/users', methods=['GET'])
+#def get_user_page():
+#    users = get_all_users()
+#    return render_template('users.html', users=users)
 
-@user_views.route('/api/users')
-def client_app():
-    users = get_all_users_json()
-    return jsonify(users)
+#@user_views.route('/api/users')
+#def client_app():
+#    users = get_all_users_json()
+#    return jsonify(users)
 
-@user_views.route('/api/lol')
-def lol():
-    return 'lol'
+#@user_views.route('/api/lol')
+#def lol():
+#    return 'lol'
 
-@user_views.route('/static/users')
-def static_user_page():
-  return send_from_directory('static', 'static-user.html')
+#@user_views.route('/static/users')
+#def static_user_page():
+#  return send_from_directory('static', 'static-user.html')
